386a3f2b52726d52a219377bb6159e97bf5c0bdd
Your name <Your Name>
2022-07-04 15:25:14 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,8 @@ float f;
 long int b;
 printf("Size of a char: %lu byte(s)\n", sizeof(d));
 printf("Size of an int: %lu byte(s)\n", sizof(a));
-printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
 printf("Size of float: %lu byte(s)\n", sizeof(int));
 return (0);
 }
