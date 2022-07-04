f3ce498fb95a25bfe3ce549e4f927357ff956d4b
Your name <Your Name>
2022-07-04 15:37:26 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,11 @@
  * main - prints the size of various type
  * Return: returns the value 0 if exited properly, non-zero otherwise
  */
-int main(void)
-{int a;
-long long int c;
-char d;
-float f;
-long int b;
-printf("Size of a char: %lu byte(s)\n", sizeof(d));
-printf("Size of an int: %lu byte(s)\n", sizof(a));
-printf("Size of a long int: %lu byte(s)\n", sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
-printf("Size of float: %lu byte(s)\n", sizeof(int));
+int main (void)
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
